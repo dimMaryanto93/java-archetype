29758cadb22cd5a7bba1cdfe0c7c8ce70abc072d
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-08-24 17:13:26 -0700

    update project

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,11 +1,6 @@
-package $com.maryanto.dimas.example;
+package ${groupId};
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World! dfghdgfhgdfhfdgh" );
